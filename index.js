c6d383086605bad95a68c0304ac833e3fa0936cd
Nihaal Rathi <nihaal.rathi23@vit.edu>
2025-09-06 16:53:08 +0530

    Enable CORS in Dealer Pricing microservice

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");  // import CORS
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());  // enable CORS for all routes
 
 const dealers = [
-  {id: 1, name: "Dealer A", productId: 1, price: 1000},
-  {id: 2, name: "Dealer B", productId: 1, price: 950},
-  {id: 3, name: "Dealer C", productId: 2, price: 500}
+  { id: 1, productId: 1, name: "Dealer A", price: 1000 },
+  { id: 2, productId: 1, name: "Dealer B", price: 950 },
+  { id: 3, productId: 2, name: "Dealer C", price: 500 }
 ];
 
 app.get("/dealers", (req, res) => {
-    res.json(dealers);
+  res.json(dealers);
 });
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log("Dealer Pricing Microservice running");
+app.listen(PORT, () => {
+  console.log(`Dealer Pricing service running on port ${PORT}`);
 });
